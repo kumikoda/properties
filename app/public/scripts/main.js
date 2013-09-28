41990f8f816c52796786edfe58ad3ffc3f63b44e
Anson Chu <anson.ch@gmail.com>
2013-09-28 12:10:22 -0700

    configured google maps api

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,4 +1,4 @@
-var properties;
+var initialize, properties;
 
 properties = [
   {
@@ -16,4 +16,14 @@ properties = [
   }
 ];
 
-console.log(properties);
+initialize = function() {
+  var map, mapOptions;
+  mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+};
+
+google.maps.event.addDomListener(window, "load", initialize);
--- app/src/scripts/main.coffee
+++ app/src/scripts/main.coffee
@@ -25,4 +25,11 @@ properties = [
 
 ]
 
-console.log properties
+initialize = ->
+  mapOptions =
+    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 8
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions)
+google.maps.event.addDomListener window, "load", initialize
