40e65ed4ea83bf14fa09a1d1d680e37f7b8ac6c9
Anson Chu <anson.ch@gmail.com>
2013-09-28 13:11:22 -0700

    added polygons

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,4 +1,4 @@
-var initialize, properties;
+var createCoords, createPolygon, initialize, properties;
 
 properties = [
   {
@@ -16,14 +16,44 @@ properties = [
   }
 ];
 
+createCoords = function(points) {
+  var point, _i, _len, _results;
+  _results = [];
+  for (_i = 0, _len = points.length; _i < _len; _i++) {
+    point = points[_i];
+    _results.push(new google.maps.LatLng(point[0], point[1]));
+  }
+  return _results;
+};
+
+createPolygon = function(coords) {
+  return new google.maps.Polygon({
+    paths: coords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35
+  });
+};
+
 initialize = function() {
-  var map, mapOptions;
-  mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  return map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  var $map, coord, map, myLatLng, polygon, property, _i, _len, _results;
+  $map = $('#map-canvas')[0];
+  myLatLng = new google.maps.LatLng(37.7749295, -122.4194155);
+  map = new google.maps.Map($map, {
+    zoom: 5,
+    center: myLatLng,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+  _results = [];
+  for (_i = 0, _len = properties.length; _i < _len; _i++) {
+    property = properties[_i];
+    coord = createCoords(property.geofences);
+    polygon = createPolygon(coord);
+    _results.push(polygon.setMap(map));
+  }
+  return _results;
 };
 
 google.maps.event.addDomListener(window, "load", initialize);
--- app/src/scripts/main.coffee
+++ app/src/scripts/main.coffee
@@ -25,11 +25,33 @@ properties = [
 
 ]
 
-initialize = ->
-  mapOptions =
-    center: new google.maps.LatLng(-34.397, 150.644)
-    zoom: 8
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+createCoords = (points) ->
+  (new google.maps.LatLng point[0] , point[1] for point in points) 
 
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions)
+createPolygon = (coords) ->
+  new google.maps.Polygon
+    paths : coords
+    strokeColor: "#FF0000"
+    strokeOpacity: 0.8
+    strokeWeight: 2
+    fillColor: "#FF0000"
+    fillOpacity: 0.35
+
+initialize = ->
+  $map = $('#map-canvas')[0]
+
+  myLatLng = new google.maps.LatLng(37.7749295, -122.4194155)
+  
+  map = new google.maps.Map $map,
+    zoom : 5
+    center : myLatLng
+    mapTypeId : google.maps.MapTypeId.TERRAIN
+
+  for property in properties
+    coord = createCoords property.geofences
+    polygon = createPolygon coord
+    polygon.setMap map
+  
+
+
 google.maps.event.addDomListener window, "load", initialize
